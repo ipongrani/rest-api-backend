d1b95e8b578ed6fed470fb91466a3636c7a7a57d
someone <someone@someplace.com>
2018-09-27 16:43:04 -0700

    nodemailer v1

--- config/smtpConfig.js
+++ config/smtpConfig.js
@@ -8,7 +8,7 @@ module.exports = (params,rand,nodemailer) => {
   let link = "http://nucleus-0.herokuapp.com/Registration?action=verify="+rand;
 
   let messageBox = "<div style='background-color: #F8F8F8; width: 300px; border: 25px solid #E05206; padding: 25px; margin: 25px;'>"+
-                       "<h1>Hello,</h1><h2>Please click on the link below to verify your email and continue login to e-lection.</h2><br>"+
+                       "<h1>Hello,</h1><h2>Please click on the link below to verify your email and continue login to Blue Care Services.</h2><br>"+
                         "<a href="+link+">https://blue-care-services.herokuapp.com</a>"+
                     "</div>";
 
